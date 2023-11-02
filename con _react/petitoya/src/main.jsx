46e04fab5088200436407055d4d9547b07df41e7
XxMaxiDiazxX <johansebastianvelezortiz@gmail.com>
2023-11-02 16:44:14 -0500

    alpha 0.0.2

--- con _react/petitoya/src/main.jsx
+++ con _react/petitoya/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import { App } from './App'
+// import Login from './componentes/login'
+import { Webos } from './Webos'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Webos />
+    </BrowserRouter>
   </React.StrictMode>,
 )
