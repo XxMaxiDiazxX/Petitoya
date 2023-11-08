7369768ce939d691497c3219547b9cc8e8e429ca
suarez025 <heljapi110@gmail.com>
2023-11-08 15:11:35 -0500

    alpha 0.0.5

---
+++ con _react/petitoya/src/componentes/BarraNavegacion.jsx
@@ -0,0 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from 'react'
+
+export const BarraNavegacion = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">inicio</Nav.Link>
+            <Nav.Link href="#link">menu</Nav.Link>
+            <Nav.Link href="#link">pedidos</Nav.Link>
+            <NavDropdown title="cuenta" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">cuenta</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                configuracion
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">cerrar sesion</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
--- con _react/petitoya/src/componentes/Login.jsx
+++ con _react/petitoya/src/componentes/Login.jsx
@@ -3,47 +3,76 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 export const Login = () => {
+
+
+  
   return (
-    <div className="container mt-5">
-      <h1>Iniciar Sesión</h1>
-      <Formik
-        initialValues={{ username: '', password: '' }}
-        validationSchema={Yup.object({
-          username: Yup.string()
-            .required('El nombre de usuario es obligatorio'),
-          password: Yup.string()
-            .required('La contraseña es obligatoria')
-        })}
-        onSubmit={(values) => {
-          console.log('Inicio de sesión exitoso:', values);
-        }}
-      >
-        <Form>
-          <div className="form-group">
-            <label htmlFor="username">Nombre de usuario</label>
-            <Field
-              type="text"
-              name="username"
-              className="form-control"
-              placeholder="Tu nombre de usuario"
-            />
-            <ErrorMessage name="username" component="div" className="text-danger" />
-          </div>
+    <div class="cont-cont">
+      <div class="contefoto"></div>
+      <div class="relleno">
+        <h4>Inicio Sesion</h4>
+        <Formik
+          initialValues={{ documento: '', contrasena: '' }}
+          validationSchema={Yup.object({
+            documento: Yup
+              .number()
+              .positive()
+              .integer()
+              .required('Usuario Requerido'),
+            contrasena: Yup
+              .string()
+              .min(6, 'La contraseña debe tener al menos 6 caractteres')
+              .matches(
+                /^(?=.*\d)(?=.*[A-Z])(?=.*[\W_]).*$/,
+                'La contraseña debe contener al menos un número, una letra mayúscula y un carácter especial'
+              )
+              .required('La Contraseña es Obligatoria'),
+          })}
+          onSubmit={(values) => {
+            console.log('Inicio de sesion Exitoso', values);
+          }}
+        >
+          <Form>
+            <div className='form-group'>
+              <label htmlFor="username">Nombre de usuario</label>
+              <Field
+                type="text"
+                name="username"
+                className="form-control"
+                placeholder="Tu Documento"
+              />
+              <ErrorMessage name="username" component="div" className="text-danger" />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="password">Contraseña</label>
-            <Field
-              type="password"
-              name="password"
-              className="form-control"
-              placeholder="Tu contraseña"
-            />
-            <ErrorMessage name="password" component="div" className="text-danger" />
-          </div>
+            <div className="form-group">
+              <label htmlFor="password">Contraseña</label>
+              <Field
+                type="password"
+                name="password"
+                className="form-control"
+                placeholder="Tu contraseña"
+              />
+              <ErrorMessage name="password" component="div" className="text-danger" />
 
-          <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
-        </Form>
-      </Formik>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
+          </Form>
+        </Formik>
+        {/* <form action="php/inicio sesion.php" class="mames" onsubmit="return validarFormulario()" method="post">
+          <input type="text" name="documento" class="usua" placeholder="Documento" required />
+          <input id="password" type="password" name="contrasena" class="contra" placeholder="Contraseña" required />
+          <span class="error" id="mensajeContrasena"></span>
+          <button type="submit">Ingresar</button>
+        </form>
+        <div class="botones-inferiores">
+          <Button variant="primary">Secondary</Button>{' '}
+          <a class="boton-registro" href="/registro">Registro</a>
+          <a class="boton-invitado" href="/">Invitado</a>
+        </div> */}
+      </div>
     </div>
   );
 }
+
+export default Login;
