1341560b8ea87ac522a391106b3fcd69c453bad9
suarez025 <heljapi110@gmail.com>
2023-11-21 18:32:18 -0500

    alpha 0.3.56

--- Cliente/con _react/petitoya/src/componentes/PieDePagina.jsx
+++ Cliente/con _react/petitoya/src/componentes/PieDePagina.jsx
@@ -9,19 +9,19 @@ export const PieDePagina = () => {
             <nav className="row justify-content-between">
 
                 <ul className='col-12 col-md-3 list-unstyled'>
-                  <li className='text-center'>ejemplo</li>
+                  <li className='text-center text-uppercase font-weight-bold'>ejemplo</li>
                   <li className='text-center'><Link to="/" className='text-reset'>Nombre_intragram</Link></li>
                   <li className='text-center'><Link to="/" className='text-reset'>Nombre_intragram</Link></li>
                 </ul>
                 
                 <ul className='col-12 col-md-3 list-unstyled'>
-                  <li className='text-center'>ejemplo</li>
+                  <li className='text-center text-uppercase font-weight-bold'>ejemplo</li>
                   <li className='text-center'><Link to="mailto:johansebastiavelezortiz@gmail.com" className='text-reset'>johansebastianvelezortiz@gmail.com</Link></li>
                   <li className='text-center'><Link to="tel:3214140078" className='text-reset'>321440078</Link></li>
                 </ul>
                 
                 <ul className='col-12 col-md-3 list-unstyled'>
-                  <li className='text-center'>ejemplo</li>
+                  <li className='text-center text-uppercase font-weight-bold'>ejemplo</li>
                   <li className='text-center'><Link to="" className='text-reset'>Misión</Link></li>
                   <li className='text-center'><Link to="" className='text-reset'>Visión</Link></li>
                   <li className='text-center'><Link to="" className='text-reset'>Términos y condicines</Link></li>
