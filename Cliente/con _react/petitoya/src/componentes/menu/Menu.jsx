e77a0a56b792d2841eb3b87b173977ad76c18df8
XxMaxiDiazxX <johansebastianvelezortiz@gmail.com>
2023-11-21 18:55:04 -0500

    Alpha 0.3.6

--- Cliente/con _react/petitoya/src/componentes/menu/Menu.jsx
+++ Cliente/con _react/petitoya/src/componentes/menu/Menu.jsx
@@ -1,36 +1,61 @@
-import { Comidas } from "./Comidas"
-import { Bebidas } from "./bebidas"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export const Menu = () => {
-    return (
-        <div className="sugerencias d-flex">
-            <div className="comidas flex-grow-1 pr-3">
-                <h4>Comidas</h4>
-                <Comidas
-                    nombre="Pasta Alfredo"
-                    descripcion="Deliciosa pasta con salsa Alfredo."
-                    precio="12.99"
-                    imagenSrc="img/cafe.jpg"
-                />
-                                <Comidas
-                    nombre="Pasta Alfredo"
-                    descripcion="Deliciosa pasta con salsa Alfredo."
-                    precio="12.99"
-                    imagenSrc="img/cafe.jpg"
-                />
+// Componente de Producto (puedes mantener tu componente existente)
+const Producto = ({ nombre, descripcion, precio, imagenSrc }) => (
+  <div className="card mb-4 box-shadow">
+    <img className="card-img-top" src={imagenSrc} alt={nombre} />
+    <div className="card-body">
+      <h5 className="card-title">{nombre}</h5>
+      <p className="card-text">{descripcion}</p>
+      <p className="card-text">{`Precio: ${precio}`}</p>
+    </div>
+  </div>
+);
 
-                {/* Agrega más carteles de comida aquí */}
-            </div>
-            <div className="bebidas red flex-grow-1 pr-3">
-                <h4>Bebidas</h4>
-                <Bebidas
-                    nombre="Refresco de Cola"
-                    descripcion="Refresco carbonatado."
-                    precio="2.99"
-                    imagenSrc="img/cafe.jpg"
-                />
-                {/* Agrega más carteles de bebidas aquí */}
-            </div>
-        </div>
-    )
-}
+// Componente Comidas (puedes tener algo similar para Bebidas)
+const Comidas = ({ productos }) => (
+  <>
+    {productos.map((producto, index) => (
+      <Producto
+        key={index}
+        nombre={producto.nombre}
+        descripcion={producto.descripcion}
+        precio={producto.precio}
+        imagenSrc={producto.imagen_url}
+      />
+    ))}
+  </>
+);
+
+const Menu = () => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    // Cambia la URL según tu entorno
+    const apiUrl = '/api/productos';
+
+    // Utiliza Axios para realizar la solicitud HTTP
+    axios.get(apiUrl)
+      .then(response => setProductos(response.data))
+      .catch(error => console.error('Error fetching productos:', error));
+  }, []);
+
+  const productosComidas = productos.filter(producto => producto.categoria === 'Comida');
+  const productosBebidas = productos.filter(producto => producto.categoria === 'Bebida');
+
+  return (
+    <div className="sugerencias d-flex">
+      <div className="comidas flex-grow-1 pr-3">
+        <h4>Comidas</h4>
+        <Comidas productos={productosComidas} />
+      </div>
+      <div className="bebidas red flex-grow-1 pr-3">
+        <h4>Bebidas</h4>
+        <Comidas productos={productosBebidas} />
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
