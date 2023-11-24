1d4a83d8ac63211270b4a7c2ade30273dae0e66d
XxMaxiDiazxX <johansebastianvelezortiz@gmail.com>
2023-11-23 23:42:56 -0500

    Alpha 0.9.10

--- Cliente/con _react/petitoya/src/componentes/BarraNavegacion.jsx
+++ Cliente/con _react/petitoya/src/componentes/BarraNavegacion.jsx
@@ -17,6 +17,11 @@ export const BarraNavegacion = () => {
     navigate('/');
   };
 
+  const EstiloCuenta = {
+    fontWeight: 'bold',
+    fontSize: '20px',
+    color: '#f2eddb'
+  }
 
   return (
     <>
@@ -24,21 +29,23 @@ export const BarraNavegacion = () => {
 
       <Navbar expand="lg" className="bg-Barra2" style={{ height: '9%' }}>
         <Container>
-          <Navbar.Brand href="#" className='red d-flex align-items-center justify-content-center'>
-            <img src={palmeraIco} height="50" className='col-md' style={{ width: '70px', float: "left" }} />
-            <p>Petitoya</p>
+          <Navbar.Brand href="#">
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <img src={palmeraIco} height="50" className='red' style={{ width: '70px' }} alt="Palmera" />
+              <p style={{ margin: 0, marginLeft: '10px', fontSize: '30px', fontWeight: 'bold' }} className='texto-titulo cuerpo'>Petitoya</p>
+            </div>
           </Navbar.Brand>
           {isLoggedIn ? (
-            <NavDropdown title="Cuenta" id="basic-nav-dropdown">
+            <NavDropdown title={<span style={EstiloCuenta}>{user.username}</span>} id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Bienvenido, {user.username}</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Configuración</NavDropdown.Item>
               <NavDropdown.Item onClick={handleLogout}>Cerrar Sesión</NavDropdown.Item>
             </NavDropdown>
           ) : (
-            <NavDropdown title="Cuenta" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/">Iniciar Sesión</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/registro">Registro</NavDropdown.Item>
-            </NavDropdown>
+            <Nav>
+              <Nav.Link as={Link} to="/registro" style={EstiloCuenta}>Registro</Nav.Link>
+              <Nav.Link as={Link} to="/" style={EstiloCuenta}>Iniciar Sesión</Nav.Link>
+            </Nav>
           )}
         </Container>
       </Navbar>
