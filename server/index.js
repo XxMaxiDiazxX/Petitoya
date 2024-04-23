92c4d2eed008c15a8219dc743495d484a49f4cc3
suarez025 <heljapi110@gmail.com>
2024-04-23 17:12:44 -0500

    Alpha 0.12.01

--- server/index.js
+++ server/index.js
@@ -5,7 +5,16 @@ const cors = require('cors');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }); // Directorio donde se guardarán los archivos subidos
 const fs = require('fs');
+const bcrypt = require('bcrypt');
 
+async function encriptarContraseña(contraseña) {
+  try {
+    const hash = await bcrypt.hash(contraseña, 10); // 10 es el número de rondas de hashing
+    return hash;
+  } catch (error) {
+    throw error;
+  }
+}
 
 app.use(cors());
 
@@ -18,7 +27,7 @@ const db = mysql.createConnection({
   database: 'petitoya'
 });
 
-app.post("/create", (req, res) => {
+/*app.post("/create", (req, res) => {
   const documento = req.body.documento;
   const nombre = req.body.nombre;
   const correo_electronico = req.body.correo_electronico;
@@ -46,9 +55,46 @@ app.post("/create", (req, res) => {
       }
     }
   );
+});*/
+
+app.post("/create", async (req, res) => {
+  const documento = req.body.documento;
+  const nombre = req.body.nombre;
+  const correo_electronico = req.body.correo_electronico;
+  const telefono = req.body.telefono;
+  const contrasena = req.body.contrasena;
+
+  try {
+    const hashedPassword = await encriptarContraseña(contrasena);
+
+    db.query('INSERT INTO clientes (id_cliente, nombre, correo_electronico, telefono, contrasena, fecha_creacion) VALUES (?,?,?,?,?,NOW())', [documento, nombre, correo_electronico, telefono, hashedPassword],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ error: 'Error interno del servidor' });
+        } else {
+          // Verifica si se insertó correctamente y devuelve información del usuario
+          if (result.affectedRows > 0) {
+            const user = {
+              documento: documento,
+              nombre: nombre,
+              correo_electronico: correo_electronico,
+              telefono: telefono,
+            };
+            res.status(200).json({ message: 'Registro exitoso', user });
+          } else {
+            res.status(500).json({ error: 'No se pudo insertar el usuario' });
+          }
+        }
+      }
+    );
+  } catch (error) {
+    console.error('Error al encriptar la contraseña:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
 });
 
-app.post("/login", (req, res) => {
+/*app.post("/login", (req, res) => {
   const { documento, contrasena } = req.body;
 
   db.query('SELECT * FROM CLIENTES WHERE id_cliente = ? AND contrasena = ?', [documento, contrasena], (err, result) => {
@@ -70,8 +116,46 @@ app.post("/login", (req, res) => {
       }
     }
   });
-});
+});*/
 
+app.post("/login", async (req, res) => {
+  const { documento, contrasena } = req.body;
+
+  try {
+    // Consulta la base de datos para obtener el hash de la contraseña almacenada
+    db.query('SELECT contrasena FROM clientes WHERE id_cliente = ?', [documento], async (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        if (result.length > 0) {
+          const hashedPassword = result[0].contrasena;
+
+          // Compara el hash almacenado con el hash de la contraseña proporcionada
+          const match = await bcrypt.compare(contrasena, hashedPassword);
+
+          if (match) {
+            // Inicio de sesión exitoso
+            const user = {
+              id: documento,
+              username: '', // Puedes agregar el nombre de usuario si lo tienes almacenado en la base de datos
+            };
+            res.status(200).json({ message: 'Inicio de sesión exitoso', user });
+          } else {
+            // Credenciales incorrectas
+            res.status(401).json({ error: 'Usuario y/o contraseña incorrectas' });
+          }
+        } else {
+          // Usuario no encontrado en la base de datos
+          res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+      }
+    });
+  } catch (error) {
+    console.error('Error al verificar la contraseña:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
 
 // Ruta para obtener todos los productos
 app.get("/productos", (req, res) => {
