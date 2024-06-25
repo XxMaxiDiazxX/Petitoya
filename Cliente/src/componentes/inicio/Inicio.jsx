e9e2b7c7b2222d5645a80948316058bbe220426b
suarez025 <heljapi110@gmail.com>
2024-06-25 00:44:00 -0500

    alpha 0.20.0

--- Cliente/src/componentes/inicio/Inicio.jsx
+++ Cliente/src/componentes/inicio/Inicio.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Producto } from "./Producto";
+import Carusel from "./Carusel";
 import palmera from '../../img/inicioSesion/fondo.png';
 import PedidoModalInicio from "./PedidoModalInicio";
 import { useAuth } from "../autenticacion/AuthContext";
 import "../../styles/Inicio/Inicio.scss"; // Importar los estilos SCSS
+import Carusel from "./Carusel";
 
 export const Inicio = () => {
   const { user } = useAuth();
@@ -45,41 +47,7 @@ export const Inicio = () => {
 
   return (
     <>
-    <div id="carouselExampleCaptions" class="carousel slide">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
+    <Carusel/>
       <div className="sugerencias container text-center">
         <h2 className="mt-3 bg-barra3 cuerpo text-white">Sugerencias</h2>
         <div className="row">
