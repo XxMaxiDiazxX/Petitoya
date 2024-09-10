1d4441c48e0f90e808daf55d1a19e6a8b8cbfa3e
XxMaxiDiazxX <johansebastianvelezortiz@gmail.com>
2024-09-10 02:00:24 -0500

    Configurar Git LFS para archivos grandes

--- server/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const adminController = require("../controllers/adminController")
-
-router.get('/consultar_superus', adminController.obtenerSuperUsuarios);
-router.delete('/eliminar_superus/:id', adminController.deleteSuperUsuarioById);
-
-module.exports = router;
